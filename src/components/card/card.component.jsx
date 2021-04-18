7ef7cc4199f2aed4b8138442a5fd2eb2716030f7
Kian <villenokian>
2021-04-18 14:59:47 +1000

    responsive and add price

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./card.styles.css"
 
-export const Card = ({sneaker}) => (
-  <div className="card-container">
-    <img className="card-container-image" alt="sneaker" src={sneaker.main_picture_url}></img>
-    <p>{sneaker.name}</p>
-  </div>
-);
+export const Card = ({sneaker}) => 
+{
+
+  const toCurrency = (num) => Number(num*0.01).toLocaleString('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  })
+
+  return (
+    <div className="card-container">
+      <img className="card-container-image" alt="sneaker" src={sneaker.main_picture_url}></img>
+      <span className="card-label-retail-price">Retail Price</span>
+      <span className="card-label-price">{toCurrency(sneaker.retail_price_cents)}</span>
+      
+      <span className="card-label-name">{sneaker.name}</span>
+    </div>
+  )
+
+}
